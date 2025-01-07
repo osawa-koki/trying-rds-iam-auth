0446a9beb6afa8ae963b4f047d072fc994231f24
osawa-koki <koki@osawa.dev>
2025-01-07 12:08:13 +0900

    データベースリソースのプロビジョニング

---
+++ lib/database/database.ts
@@ -0,0 +1,56 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import { Construct } from 'constructs';
+
+interface DatabaseStackProps extends cdk.StackProps {
+  stackName: string;
+  vpc: ec2.Vpc;
+  selectedSubnets: ec2.SelectedSubnets;
+}
+
+export default class DatabaseStack extends cdk.Stack {
+  public readonly aurora: rds.DatabaseCluster;
+
+  constructor(scope: Construct, id: string, props: DatabaseStackProps) {
+    const { stackName, vpc, selectedSubnets } = props;
+
+    super(scope, id, {
+      ...props,
+      stackName,
+    });
+
+    const aurora = new rds.DatabaseCluster(this, 'MyAurora', {
+      engine: rds.DatabaseClusterEngine.auroraMysql({
+        version: rds.AuroraMysqlEngineVersion.VER_3_04_0
+      }),
+      vpc,
+      vpcSubnets: selectedSubnets,
+      deletionProtection: false,
+      defaultDatabaseName: process.env.AURORA_DATABASE_NAME ?? 'default_database',
+      writer: rds.ClusterInstance.provisioned("Writer", {
+        instanceType: ec2.InstanceType.of(
+          // `rds.AuroraMysqlEngineVersion.VER_3_04_0`は`t3.medium`以上のみ選択可能。
+          ec2.InstanceClass.T3,
+          ec2.InstanceSize.MEDIUM
+        ),
+        publiclyAccessible: false,
+        instanceIdentifier: "db-writer",
+      }),
+      // IAM認証を有効にする。
+      iamAuthentication: true,
+      // 節約のために読み取り専用インスタンスは作成しない。
+      readers: [],
+    });
+
+    // 勉強目的のため、Auroraに対して全てのIPアドレスからのMySQL（ポート3306）接続を許可する。
+    const securityGroup = new ec2.SecurityGroup(this, 'AuroraSecurityGroup', {
+      vpc,
+      allowAllOutbound: true,
+    });
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(3306), 'Allow MySQL access from anywhere');
+    aurora.connections.addSecurityGroup(securityGroup);
+
+    this.aurora = aurora;
+  }
+}
--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 import NetworkStack from './network/network';
+import DatabaseStack from './database/database';
 
 export class IndexStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,8 +11,16 @@ export class IndexStack extends cdk.Stack {
       stackName: process.env.BASE_STACK_NAME!,
     });
 
-    new NetworkStack(this, 'NetworkStack', {
+    const networkStack = new NetworkStack(this, 'NetworkStack', {
       stackName: `${process.env.BASE_STACK_NAME!}-network`,
     });
+
+    const databaseStack = new DatabaseStack(this, 'DatabaseStack', {
+      stackName: `${process.env.BASE_STACK_NAME!}-database`,
+      vpc: networkStack.vpc,
+      selectedSubnets: networkStack.selectedSubnets,
+    });
+    databaseStack.addDependency(networkStack);
   }
 }
+
