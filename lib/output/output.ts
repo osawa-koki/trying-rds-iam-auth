ef2582811ae8e6991429180b17d84f88e465a765
osawa-koki <koki@osawa.dev>
2025-01-07 12:23:21 +0900

    アウトプットスタックの作成

---
+++ lib/output/output.ts
@@ -0,0 +1,27 @@
+import * as cdk from 'aws-cdk-lib';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import { Construct } from 'constructs';
+
+interface OutputStackProps extends cdk.StackProps {
+  stackName: string;
+  aurora: rds.DatabaseCluster;
+}
+
+export default class OutputStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: OutputStackProps) {
+    const { stackName, aurora } = props;
+
+    super(scope, id, {
+      ...props,
+      stackName,
+    });
+
+    new cdk.CfnOutput(this, 'AuroraEndpoint', {
+      value: aurora.clusterEndpoint.hostname,
+    });
+
+    new cdk.CfnOutput(this, 'AuroraPort', {
+      value: aurora.clusterEndpoint.port.toString(),
+    });
+  }
+}
