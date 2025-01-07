65556c13f32bb5afccf11ec7449126bf49c14197
osawa-koki <koki@osawa.dev>
2025-01-07 11:19:54 +0900

    ネットワークリソースのプロビジョニング

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
+import NetworkStack from './network/network';
+
 export class IndexStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, {
       ...props,
       stackName: process.env.BASE_STACK_NAME!,
+    });
+
+    new NetworkStack(this, 'NetworkStack', {
+      stackName: `${process.env.BASE_STACK_NAME!}-network`,
     });
   }
 }
