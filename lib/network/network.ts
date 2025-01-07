013b5a587289e1b78af9c0dc69b84952fc931bc2
osawa-koki <koki@osawa.dev>
2025-01-07 11:21:53 +0900

    small fix

--- lib/network/network.ts
+++ lib/network/network.ts
@@ -19,7 +19,7 @@ export default class NetworkStack extends cdk.Stack {
 
     const vpc = new ec2.Vpc(this, 'MyVpc', {
       maxAzs: 2,
-      natGateways: 1,
+      natGateways: 0,
       subnetConfiguration: [
         {
           cidrMask: 24,
